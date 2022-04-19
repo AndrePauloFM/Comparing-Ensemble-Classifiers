408eadf0c0cbda8f7217e0fd7884338dad737376
AndrePauloFM <50689018+AndrePauloFM@users.noreply.github.com>
2022-04-19 02:03:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""*Primeiro Trabalho de Inteligência Artificial e Sistemas Inteligentes - Combinado de Classificadores*
+"""*Combinado de Classificadores*
 
 *André Paulo Ferreira Machado*
 
